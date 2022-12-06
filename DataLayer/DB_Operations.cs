c6ac782225000f0000c81daf30451f9be972bfe3
Thilakshi Perera <86902937+Thishi98@users.noreply.github.com>
2022-12-06 12:57:55 +0530

    Second Commit

---
+++ DataLayer/DB_Operations.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace DataLayer
+{
+    public class DB_Operations
+    {
+        private static MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=");
+
+        public static int ExcecuteQuery(string MySql)
+        {
+            try
+            {
+                MySqlCommand command = new MySqlCommand(MySql, connection);
+                connection.Open();
+                return command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        public static DataSet ExcSelectQuery(String mysql)
+        {
+            try
+            {
+                MySqlDataAdapter myda = new MySqlDataAdapter(mysql,connection);
+                connection.Open();
+                DataSet ds = new DataSet();
+                myda.Fill(ds);
+                return ds;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally 
+            {
+                connection.Close();
+            }
+        }
+     }
+}
